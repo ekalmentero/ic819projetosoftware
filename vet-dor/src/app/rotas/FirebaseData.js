316944acc8376bd14e7d4c84d234c85d03709c92
maxcravo <maxcravo25@gmail.com>
2023-07-04 11:31:19 -0300

    fazendo rota de getuser

--- vet-dor/src/app/rotas/FirebaseData.js
+++ vet-dor/src/app/rotas/FirebaseData.js
@@ -1,16 +1,13 @@
 const admin = require("firebase-admin");
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const { getFirestore } = require('firebase-admin/firestore');
 const ServiceAccount = require("./projetovet-dor-firebase-adminsdk-jddnl-8f04ade30e.json");
-
 const cors = require('cors')
 
 admin.initializeApp({
   credential: admin.credential.cert(ServiceAccount),
   // databaseURL: "https://projetovet-dor-default-rtdb.firebaseio.com"
 });
-
-// admin.initializeApp();
 
 const express = require("express");
 const app = express();
@@ -24,6 +21,7 @@ app.use(cors());
 
 const PORT = 8080;
 
+// rota criar usuário
 app.post('/createUser', async (req, res) =>{
   console.log("[/createUser]");
   console.log(`req.body = ${JSON.stringify(req.body)}`);
@@ -36,9 +34,9 @@ app.post('/createUser', async (req, res) =>{
       email: req.body.emailLimpo
     };
 
-    const username = jsonData.name.trim().toLowerCase;
+    const cpf = jsonData.cpf;
 
-    const userRef = await database.collection('users').doc(username).set(jsonData);
+    const userRef = await database.collection('users').doc(cpf).set(jsonData);
     console.log(`userRef = ${JSON.stringify(userRef)}`);
     res.send(userRef);
 
@@ -48,7 +46,33 @@ app.post('/createUser', async (req, res) =>{
   }
 })
 
- 
+// rota pegar dados de um usuário, utilizando o CPF como chave 
+// https://firebase.google.com/docs/firestore/query-data/get-data -> getUser document
+app.get('/getUser', async (req, res) => {
+  try {
+    console.log("[/getUser]");
+    const cpf = req.body.cpfLimpo;
+
+    const docRef = doc(database, 'users', cpf);
+    const userFound = await getDoc(docRef);
+    
+    if(userFound.exists()) {
+      console.log(`usuário existe - ${userFound.data()}`);
+      res.status(200).send({
+        userData: userFound.data()
+      })
+    } else {
+      console.log("usuário não existe");
+      res.status(404).send({
+        message: "usuário não existe"
+      })
+    }
+  } catch (error) {
+    console.log(`/getUser error = ${error}`);
+  }
+})
+
+
 // app.get('/user', (req,res)=>{
 //   admin.firestore().collection('users').get().then(snapshot =>{
 //     const data = snapshot.docs.map(doc=>({
